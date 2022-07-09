8427b4d3404e44dc285cfb6c48a78d09ac870ac4
Kartik katiyar <kartikktyr@gmail.com>
2022-07-09 13:03:57 +0530

    Updated image carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,9 @@
 import React from "react";
 import logo from "../images/logo.png";
 import "./Carousel.css";
-import img1 from "../images/img1.jpg";
+import img1 from "../images/1720.jpg";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { MdMinimize } from "react-icons/md";
 const Carousel = () => {
   return (
     <div className="c">
@@ -10,32 +12,25 @@ const Carousel = () => {
         className="carousel slide"
         data-ride="carousel"
       >
-        <ol className="carousel-indicators">
+        <ol className="carousel-indicators" color="#fff" zIndex="2">
           <li
             data-target="#carouselExampleIndicators"
             data-slide-to="0"
             className="active"
           ></li>
+
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img className="d-block" src={img1} alt="First slide" />
+            <img className="d-block  image" src={img1} alt="First slide" />
           </div>
           <div className="carousel-item">
-            <img
-              className="d-block "
-              src="https://images.pexels.com/photos/927497/pexels-photo-927497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              alt="Second slide"
-            />
+            <img className="d-block" src={img1} alt="Second slide" />
           </div>
           <div className="carousel-item">
-            <img
-              className="d-block"
-              src="https://images.pexels.com/photos/2765872/pexels-photo-2765872.jpeg"
-              alt="Third slide"
-            />
+            <img className="d-block image " src={img1} alt="Third slide" />
           </div>
         </div>
         <a
@@ -44,13 +39,11 @@ const Carousel = () => {
           role="button"
           data-slide="prev"
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
+          <IoIosArrowBack color="#ffffff" zIndex="2" fontSize={"40px"} />
           <span className="sr-only">Previous</span>
         </a>
-        <a
+
+        {/* <a
           className="carousel-control-next"
           href="#carouselExampleIndicators"
           role="button"
@@ -61,6 +54,15 @@ const Carousel = () => {
             aria-hidden="true"
           ></span>
           <span className="sr-only">Next</span>
+        </a> */}
+        <a
+          className="carousel-control-next "
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <IoIosArrowForward color="#ffffff" zIndex="2" fontSize={"40px"} />
+          <span className="sr-only">Previous</span>
         </a>
       </div>
     </div>
